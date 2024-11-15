98fcd6fe198f01d024a37705d7e56c7d0798418c
Konstantina Karantza <t8230244@aueb.gr>
2024-11-16 01:14:50 +0200

    Προσθήκη ΑΜ μέλους 3

--- TeamIDs.java
+++ TeamIDs.java
@@ -2,5 +2,6 @@ public class TeamIDs {
     public static void main (String[] args) {
         System.out.println("ΑΜ μέλους 1: 8220011");
         System.out.println("ΑΜ μέλους 2: 8230107");
+        System.out.println("ΑΜ μέλους 2: 8230244");
     }
 }
