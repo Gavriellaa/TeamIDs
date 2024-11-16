c0fc9b5d17e0a244835b5ba0594d29e3e2ee6867
mantalenastavrakaki <t8220142@aueb.gr>
2024-11-16 12:48:35 +0200

    Προσθήκη ΑΜ μέλους 5

--- TeamIDs.java
+++ TeamIDs.java
@@ -2,7 +2,8 @@ public class TeamIDs {
     public static void main (String[] args) {
         System.out.println("ΑΜ μέλους 1: 8220011");
         System.out.println("ΑΜ μέλους 2: 8230107");
-        System.out.println("ΑΜ μέλους 2: 8230244");
+        System.out.println("ΑΜ μέλους 3: 8230244");
         System.out.println("ΑΜ μέλους 4: 8220052");
+        System.out.println("ΑΜ μέλους 5: 8220142");
     }
 }
