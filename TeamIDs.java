f1b62259bff5e4d72c3877bd604130f13c6d14b9
dimitrismart <148975180+dimitrismart@users.noreply.github.com>
2024-11-17 14:35:34 +0200

    Προσθήκη ΑΜ Μέλους 8

--- TeamIDs.java
+++ TeamIDs.java
@@ -1,5 +1,5 @@
 public class TeamIDs {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         System.out.println("ΑΜ μέλους 1: 8220011");
         System.out.println("ΑΜ μέλους 2: 8230107");
         System.out.println("ΑΜ μέλους 3: 8230244");
@@ -7,7 +7,7 @@ public class TeamIDs {
         System.out.println("ΑΜ μέλους 5: 8220142");
         System.out.println("ΑΜ μέλους 6: 8230059");
         System.out.println("ΑΜ μέλους 7: 8230153");
-
+        System.out.println("ΑΜ μέλους 8: 8220086");
 
     }
 }
